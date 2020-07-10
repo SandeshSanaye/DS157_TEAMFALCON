4819d749afc191a21c9a885ac04ac27ce5cd9293
sandeshsanaye <sanayesandesh@gmail.com>
2020-07-10 11:19:53 +0000

    change installation scripts

--- vagrant/node.sh
+++ vagrant/node.sh
@@ -1,1 +1,12 @@
-apt-get install nodejs
+rm -rf ~/.nvm
+apt-get -y install build-essential libssl-dev
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
+[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"
+a
+nvm install v10.20.1
+nvm use v10.20.1
+
+source ~/.profile
